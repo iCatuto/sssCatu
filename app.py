9fd3a1d8edc5f65155e2cff4e11cb9927f5b0b6a
iCatuto <111618918+iCatuto@users.noreply.github.com>
2025-05-01 20:14:31 -0500

    Spotdl app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, request, jsonify
+import os
+import subprocess
+import uuid
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "Servidor Flask para descargas funcionando."
+
 @app.route("/descarga", methods=["POST"])
 def descarga():
     try:
@@ -34,3 +45,7 @@ def descarga():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
